26098122c8e55acf7d2e26e80ec8a0ef51f6e13b
lyp3238 <lyp3238@gmail.com>
2020-12-05 03:29:17 +0900

    Boss 점프 착지 기능을 제작함

--- Assets/Game/101. Scripts/AIPrototype/AIMaster.cs
+++ Assets/Game/101. Scripts/AIPrototype/AIMaster.cs
@@ -280,6 +280,37 @@ public class AIMaster : MonoBehaviour
     //    isDecreaseGroggy = false;
     //}
 
+    public Vector3 jumpTarget;
+    public float speed = 50f;
+    public bool checkArrive = false;
+    public void StartJumpToTarget()
+    {
+        jumpTarget = player.transform.position;
+        jumpTarget.y = 0f;
+        speed = 50f;
+        checkArrive = false;
+    }
+
+    public void UpdataJumpToTarget()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        Vector3 pos = transform.position;
+        pos.y = 0f;
+        if (Vector3.Distance(pos, jumpTarget) <= 15f)
+        {
+            checkArrive = true;
+        }
+        if (checkArrive)
+        {
+            speed = Mathf.Lerp(speed, 0, Time.deltaTime * 3f);
+            if (speed <= 10f)
+            {
+                anim.SetTrigger("timerTrigger");
+            }
+        }
+    }
+
     #region Evade Function
     public void SetEvadePosition(out bool value)
     {
