58598472a3df82786185bd0f86ef66230b8f18c7
lyp3238 <lyp3238@gmail.com>
2020-12-08 22:42:23 +0900

    스테미나 기능 추가

---
+++ Assets/Game/101. Scripts/UI/PlayerHud/UIActionPoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIActionPoint : UIHPBaseClass
+{
+    public PlayerCharacterStatus player;
+
+    protected override void Start()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCharacterStatus>();
+        }
+        maxHp = player.Stamina;
+        currentHp = maxHp;
+    }
+
+    protected override void SetCurrentHpBarSetting()
+    {
+        currentHp = player.Stamina;
+    }
+}
