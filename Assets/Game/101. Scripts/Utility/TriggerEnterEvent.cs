8b1fa16c743d1430678df6b4d69425a49594bed5
lyp3238 <lyp3238@gmail.com>
2020-12-09 21:15:17 +0900

    TriggerEnterEvent 스크립트 추가 - 컷신용

---
+++ Assets/Game/101. Scripts/Utility/TriggerEnterEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEnterEvent : MonoBehaviour
+{
+    public UnityEvent enterEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        enterEvent.Invoke();
+    }
+}
