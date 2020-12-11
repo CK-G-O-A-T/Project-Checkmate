418df5f4042010c8ccc0a7a759bb11b126413f76
lyp3238 <lyp3238@gmail.com>
2020-12-11 23:28:30 +0900

    병합을 위한 설정을 했음

--- Assets/Game/101. Scripts/UI/PlayerHud/UIBossHP.cs
+++ Assets/Game/101. Scripts/UI/PlayerHud/UIBossHP.cs
@@ -14,10 +14,22 @@ public class UIBossHP : UIGaugebar
 
         damageEvent = aiMaster.GetComponent<BossDamageHandler>();
         damageEvent.damageEvent.AddListener(UpdateGauge);
+
+        StartCoroutine(LerpMainGauge());
     }
 
     protected override void SetCurrentGaugeSetting()
     {
         currentValue = aiMaster.healthPoint;
+    }
+
+    IEnumerator LerpMainGauge()
+    {
+        while (mainGaugeBar.fillAmount < 0.999f)
+        {
+            //mainGaugeBar.fillAmount = Mathf.Lerp(mainGaugeBar.fillAmount, 1, Time.deltaTime * 0.7f);
+            mainGaugeBar.fillAmount += 0.2f * Time.deltaTime;
+            yield return null;
+        }
     }
 }
