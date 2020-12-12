4fad6b9c15d66f66c15e92388f77b98cfcebaafe
SteamB23 <steamb23@outlook.com>
2020-12-12 20:24:00 +0900

    플레이어 캐릭터 파티클 이펙트 프리팹 병합

---
+++ Assets/Game/101. Scripts/PcvfxManager.cs
@@ -0,0 +1,48 @@
+﻿//윤표꺼의 보스이펙트소환을 똑같이 적용함
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PceffectStruct
+{
+    public ParticleSystem particleObject;
+    [HideInInspector]
+    public GameObject particleGameObject;
+}
+
+
+
+public class PcvfxManager : MonoBehaviour
+{
+    public List<PceffectStruct> Pceffects;
+    public Transform effectPosition;
+    public float effectDuration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < Pceffects.Count; i++)
+        {
+            Pceffects[i].particleGameObject = Pceffects[i].particleObject.gameObject;
+            Pceffects[i].particleGameObject.SetActive(false);
+        }
+    }
+
+    public void Effect_Start(int index)
+    {
+        Pceffects[index].particleGameObject.SetActive(true);
+        Pceffects[index].particleObject.Play();
+        //effects[index].particleGameObject.transform.position = effectPosition.position;
+        //effects[index].particleGameObject.transform.rotation = Quaternion.identity;
+        StartCoroutine(DestroyParticle(Pceffects[index].particleGameObject, Pceffects[index].particleObject.duration));
+        Debug.Log("Effect Index : " + index);
+    }
+
+    IEnumerator DestroyParticle(GameObject gb, float particleDuration)
+    {
+        yield return new WaitForSeconds(particleDuration);
+        gb.SetActive(false);
+    }
+}
