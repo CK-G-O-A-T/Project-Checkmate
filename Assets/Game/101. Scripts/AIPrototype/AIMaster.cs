3d53564381b00bab8ee2b4447af207c276f0e3d0
lyp3238 <lyp3238@gmail.com>
2021-01-05 15:22:54 +0900

    Jump Attack 부분을 AIMaster에서 분리하고 단독 스크립트로 만듬 리팩토링 #2

--- Assets/Game/101. Scripts/AIPrototype/AIMaster.cs
+++ Assets/Game/101. Scripts/AIPrototype/AIMaster.cs
@@ -23,6 +23,9 @@ public class AIMaster : MonoBehaviour
     [Header("Groggy")]
     public BossGroggyComponent groggyComponent;
 
+    [Header("Jump Attack")]
+    public BossJumpAttackComponent jumpAttackComponent;
+
     [Header("Distance Setting")]
     public float closeRangeAttackDistance;
     public float longRangeAttackDistance;
@@ -34,8 +37,8 @@ public class AIMaster : MonoBehaviour
 
     private NavMeshAgent agent;
     private GameObject player;
-    private float speedSave;
     [HideInInspector] public Animator anim;
+    private float speedSave;
 
     [HideInInspector] public bool isEvade = false;
     [HideInInspector] public bool isMove = true;
@@ -47,6 +50,14 @@ public class AIMaster : MonoBehaviour
         set
         {
             agent.destination = value;
+        }
+    }
+
+    public GameObject getPlayer
+    {
+        get
+        {
+            return player;
         }
     }
 
@@ -221,37 +232,6 @@ public class AIMaster : MonoBehaviour
     public void SetBool(string name)
     {
         anim.SetBool(name, true);
-    }
-
-    public Vector3 jumpTarget;
-    public float speed = 50f;
-    public bool checkArrive = false;
-    public void StartJumpToTarget()
-    {
-        jumpTarget = player.transform.position;
-        jumpTarget.y = 0f;
-        speed = 50f;
-        checkArrive = false;
-    }
-
-    public void UpdataJumpToTarget()
-    {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-
-        Vector3 pos = transform.position;
-        pos.y = 0f;
-        if (Vector3.Distance(pos, jumpTarget) <= 15f)
-        {
-            checkArrive = true;
-        }
-        if (checkArrive)
-        {
-            speed = Mathf.Lerp(speed, 0, Time.deltaTime * 4f);
-            if (speed <= 10f)
-            {
-                anim.SetTrigger("timerTrigger");
-            }
-        }
     }
 
     public void PlayHitSound()
--- Assets/Game/101. Scripts/AIPrototype/FSM/JumpToTarget.cs
+++ Assets/Game/101. Scripts/AIPrototype/FSM/JumpToTarget.cs
@@ -9,13 +9,13 @@ public class JumpToTarget : StateMachineBehaviour
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         aiMaster = animator.GetComponent<AIMaster>();
-        aiMaster.StartJumpToTarget();
+        aiMaster.jumpAttackComponent.StartJumpToTarget();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        aiMaster.UpdataJumpToTarget();
+        aiMaster.jumpAttackComponent.UpdataJumpToTarget();
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
